a476f4f8ad6f07d83939e00ad41507c33010c3bf
asir-op <asiransari55@gmail.com>
2024-03-07 02:40:33 +0530

    changed the image not showing error

--- src/components/Rolldice.jsx
+++ src/components/Rolldice.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// import dice from '../assets/dice/dice_{currentDice}.png'
 
 function Rolldice({currentDice, RollTheDice}) {
     
@@ -7,7 +8,7 @@ function Rolldice({currentDice, RollTheDice}) {
   return (
     <>
     <div className='flex flex-col items-center gap-2 mt-4'>
-        <img src={`public/dice/dice_${currentDice}.png`} alt="dice 1" className='cursor-pointer'  onClick={() => RollTheDice()}/>
+        <img src={`dice/dice_${currentDice}.png`} alt="dice 1" className='cursor-pointer'  onClick={() => RollTheDice()}/>
         <p className='text-2xl '>Click on Dice to Roll</p>
         
     </div>

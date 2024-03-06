05e7437e82331845e719f5d9f2caef2ae57f602e
asir-op <asiransari55@gmail.com>
2024-03-07 01:35:28 +0530

    fixed vite config error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Dice Game using react/"
+  base: "/Dice Game using react/",
   plugins: [react()],
 })
